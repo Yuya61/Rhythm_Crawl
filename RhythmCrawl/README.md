551a2cf161d7e07c45b99c3f9bdc1a3a6a871902
Yuya Hashirizaki <hashirizaki61@gmail.com>
2020-12-09 23:54:52 -0800

    fetch code

--- RhythmCrawl/README.md
+++ RhythmCrawl/README.md
@@ -1,6 +1,52 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Webpod
 
-## Available Scripts
+![webpod_banner](public/assets/webpod.png)
+
+[![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+![GitHub package.json dependency version (prod)](https://img.shields.io/github/package-json/dependency-version/dipto0321/foody-recipe-app-fe/react)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/anuragbhattacharjee/webpod)
+![GitHub last commit (branch)](https://img.shields.io/github/last-commit/anuragbhattacharjee/webpod/master)
+![GitHub](https://img.shields.io/github/license/anuragbhattacharjee/webpod)
+
+A Web App to play distraction free music.
+
+## Table of Contents
+
+- [webpod](#webpod)
+  - [Table of Contents](#table-of-contents)
+  - [Instructions](#insuctions)
+  - [Install](#install)
+  - [Usage](#usage)
+    - [`npm start`](#yarn-start)
+    - [`npm test`](#yarn-test)
+    - [`npm build`](#yarn-build)
+  - [API](#api)
+  - [Maintainers](#maintainers)
+  - [Contributing](#contributing)
+  - [License](#license)
+
+## Instructions:
+
+-!search music_name: - A search list will appear - Only enter the number of the song you want to listen. - Example: 3
+-!play music_name - to play a music
+-!pause - to pause current playing music
+-!stop - to stop playing
+-!volume desired_volume - To set volume - Example: !volume 71
+
+## Background
+
+## Install
+
+In the project directory, run:
+
+```
+npm install
+```
+
+## Usage
+
+You need to create your own API credential on [Google Developer Console](https://console.developers.google.com/apis/credentials).
+Rename .env.sample to .env and replace API_KEY value to your API Key.
 
 In the project directory, you can run:
 
@@ -12,12 +58,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
+### `npm build`
 
 Builds the app for production to the `build` folder.<br />
 It correctly bundles React in production mode and optimizes the build for the best performance.
@@ -27,42 +68,16 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+## Maintainers
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+[@anuragbhattacharjee](https://github.com/anuragbhattacharjee)
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Contributing
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+See [the contributing file](contributing.md)!
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+PRs accepted.
 
-## Learn More
+## License
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+MIT © 2020 Anurag Bhattacharjee
--- RhythmCrawl/src/App.js
+++ RhythmCrawl/src/App.js
@@ -1,26 +1,242 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import _ from "lodash";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// For dark mode
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./services/theme";
+import { GlobalStyle } from "./services/global";
+
+import Navbar from "./components/navbar";
+import Player from "./components/player";
+import Instructions from "./components/instructions/index";
+import Command from "./components/command";
+
+import youtube, { baseTerms } from "./services/youtube";
+
+require("dotenv").config();
+
+// document.body.style.backgroundColor = "#282c34";
+
+class App extends Component {
+  state = {
+    inputTerm: "",
+    commands: [],
+    videos: [],
+    selectedVideo: null,
+    instruction: "",
+    vdoEvent: null,
+    vdoPauseTime: 0,
+    theme: "dark",
+  };
+
+  handleVideoEvent = (c) => {
+    this.setState({
+      vdoEvent: c,
+    });
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      inputTerm: event.target.value,
+    });
+  };
+
+  handleCommandSubmit = async (event) => {
+    event.preventDefault();
+
+    // For displaying user inputs
+    let commands = [...this.state.commands];
+    let selectedVideo = { ...this.state.selectedVideo };
+    let videos = [...this.state.videos];
+    const vdoEvent = this.state.vdoEvent;
+    let instruction = "";
+    let searchTerm = "";
+
+    const inputTerm = this.state.inputTerm.toLowerCase();
+
+    if (!isNaN(inputTerm) && !_.isEmpty(videos)) {
+      if (!_.isEmpty(selectedVideo)) {
+        this.stopVideo();
+      }
+      let index = parseInt(inputTerm);
+      if (index > videos.length) index = videos.length;
+      selectedVideo = { ...videos[index - 1] };
+      videos = []; // For not showing Search list in the menu anymore
+      if (!_.isEmpty(vdoEvent)) {
+        vdoEvent.target.loadVideoById(selectedVideo.id.videoId);
+      }
+      commands.push(`Playing ${selectedVideo.snippet.title}`);
+      instruction = "";
+      searchTerm = "";
+    } else {
+      instruction = inputTerm.match(/!\w+/g);
+      if (instruction)
+        instruction = instruction.length > 0 ? instruction[0] : "";
+
+      searchTerm = instruction
+        ? inputTerm.toLowerCase().split(instruction)[1].trim()
+        : "";
+    }
+
+    if (instruction === "!volume") {
+      this.setVolume(parseInt(searchTerm));
+      commands.push(`Volume set to: ${searchTerm}%`);
+    } else if (instruction === "!play" || instruction === "!search") {
+      if (searchTerm !== "") videos = await this.searchVideo(searchTerm);
+    }
+
+    switch (instruction) {
+      case "!play":
+        if (searchTerm === "") {
+          const videoResumed = this.resumeVideo();
+          const msg = videoResumed
+            ? `Resuming ${selectedVideo.snippet.title}`
+            : "No music on the list.";
+          commands.push(msg);
+        } else {
+          if (!_.isEmpty(selectedVideo)) {
+            this.stopVideo();
+          }
+          selectedVideo = { ...videos[0] };
+          videos = []; // For not showing Search list in the menu
+          if (!_.isEmpty(vdoEvent)) {
+            vdoEvent.target.loadVideoById(selectedVideo.id.videoId);
+          }
+          commands.push(`Playing ${selectedVideo.snippet.title}`);
+        }
+        break;
+      case "!pause":
+        this.pauseVideo();
+        break;
+      case "!resume":
+        const videoResumed = this.resumeVideo();
+        const resumeMsg = videoResumed
+          ? `Resuming ${selectedVideo.snippet.title}`
+          : "No music on the list.";
+        commands.push(resumeMsg);
+        break;
+      case "!stop":
+        const stoppedVideo = this.stopVideo();
+        if (stoppedVideo) {
+          commands.push(`Stopped playing ${selectedVideo.snippet.title}`);
+          selectedVideo = null;
+        } else {
+          commands.push("No music to stop.");
+        }
+        break;
+      default:
+        break;
+    }
+
+    commands = commands.reverse();
+    // localStorage.setItem("commands", commands);
+
+    this.setState({
+      inputTerm: "",
+      commands,
+      videos,
+      selectedVideo,
+    });
+  };
+
+  searchVideo = async (searchTerm) => {
+    console.log("Process key: ", process.env);
+    const response = await youtube.get("/search", {
+      params: {
+        ...baseTerms,
+        q: searchTerm,
+      },
+    });
+    return response.data.items;
+  };
+
+  setVolume = (volume) => {
+    const { vdoEvent } = this.state;
+    if (!_.isEmpty(vdoEvent)) {
+      vdoEvent.target.setVolume(volume);
+      this.setState({
+        vdoPauseTime: vdoEvent.target.getDuration(),
+      });
+    }
+  };
+
+  pauseVideo = () => {
+    const { vdoEvent } = this.state;
+    if (!_.isEmpty(vdoEvent)) {
+      vdoEvent.target.pauseVideo();
+      this.setState({
+        vdoPauseTime: vdoEvent.target.getDuration(),
+      });
+    }
+  };
+
+  resumeVideo = () => {
+    const { vdoEvent } = this.state;
+    if (!_.isEmpty(vdoEvent)) {
+      vdoEvent.target.playVideo();
+      return 1;
+    }
+    return 0;
+  };
+
+  stopVideo = () => {
+    const { vdoEvent } = this.state;
+    if (!_.isEmpty(vdoEvent)) {
+      vdoEvent.target.stopVideo();
+      // this.setState({
+      //   vdoEvent: null,
+      // });
+      return 1;
+    }
+    return 0;
+  };
+
+  // The function that toggles between themes
+  toggleTheme = () => {
+    // if the theme is not light, then set it to dark
+    if (this.state.theme === "light") {
+      this.setState({ theme: "dark" });
+      // otherwise, it should be light
+    } else {
+      this.setState({ theme: "light" });
+    }
+  };
+
+  render() {
+    const { inputTerm, videos, selectedVideo, commands, theme } = this.state;
+
+    return (
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <>
+          <GlobalStyle />
+          <main className="container-fluid d-flex h-100 flex-column">
+            <Navbar />
+            <div className="row main-container flex-fill">
+              <div className="col-3 main-instructions-column">
+                <Instructions theme={theme} onToggleTheme={this.toggleTheme} />
+              </div>
+              <div className="col-6 main-player-column">
+                <Player
+                  video={selectedVideo}
+                  onVideoEvent={(c) => this.handleVideoEvent(c)}
+                />
+              </div>
+              <div className="col-3 main-command-column">
+                <Command
+                  inputTerm={inputTerm}
+                  videos={videos}
+                  commands={commands}
+                  onInputChange={this.handleInputChange}
+                  onCommandSubmit={this.handleCommandSubmit}
+                />
+              </div>
+            </div>
+          </main>
+        </>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
